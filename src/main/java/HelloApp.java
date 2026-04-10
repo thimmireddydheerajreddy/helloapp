34fefa399ed357f3c37c8ee3728f18bca4b0be59
thimmireddydheerajreddy <thimmireddydheeraj@gmail.com>
2026-04-10 14:19:17 +0530

    UC4

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -2,10 +2,15 @@ public class HelloApp {
     public static void main(String[] args) {
         String name = "World";
 
-        if (args.length > 0){
-            name = args[0];
+        StringBuilder nameBuilder = new StringBuilder();
+        for (int i = 0; i < args.length; i++) {
+            nameBuilder.append(args[i]);
+            if (i < args.length - 1) {
+                nameBuilder.append(", ");
+            }
+            name = nameBuilder.toString();
         }
-        
+
         System.out.println("Hello, " + name + "!");
     }
 }
