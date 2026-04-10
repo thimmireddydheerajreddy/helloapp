2efd7bfc251c9eb0f7915509522628db704a214d
thimmireddydheerajreddy <thimmireddydheeraj@gmail.com>
2026-04-10 14:21:50 +0530

    UC6

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,5 +1,21 @@
 public class HelloApp {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+            return;
+        }
+
+        StringBuilder nameBuilder = new StringBuilder();
+        
+        for (String arg : args) {
+            nameBuilder.append(arg).append(", ");
+        }
+
+        String names = "";
+        if (nameBuilder.length() > 0) {
+            names = nameBuilder.substring(0, nameBuilder.length() - 2);
+        }
+
+        System.out.println("Hello, " + names + "!");
     }
 }
